988e55f65dfb135df86e871c313eae9aee732c8b
Raghib Hasan <raghib.hasan@codemate.com>
2020-01-20 17:32:39 +0200

    Updates

---
+++ precache-manifest.e5f3de96d7a964291ac178f4472c2abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2d2d919f9fa1911e8f5cd95b6831d6",
+    "url": "/choco-bar-client/index.html"
+  },
+  {
+    "revision": "510ce96733b0e8daa081",
+    "url": "/choco-bar-client/static/css/main.63de37f9.chunk.css"
+  },
+  {
+    "revision": "cf7d53963a16dd5191c8",
+    "url": "/choco-bar-client/static/js/2.f46d3b85.chunk.js"
+  },
+  {
+    "revision": "4c76d08543145e9206fa99d1177e90e1",
+    "url": "/choco-bar-client/static/js/2.f46d3b85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "510ce96733b0e8daa081",
+    "url": "/choco-bar-client/static/js/main.f70229f1.chunk.js"
+  },
+  {
+    "revision": "607e80a82295aa48560d",
+    "url": "/choco-bar-client/static/js/runtime-main.af9b4c7b.js"
+  }
+]);
